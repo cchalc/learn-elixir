e1b9250f7d939e22f5f2d712a7b97af5c2384689
Chris Chalcraft <chris.chalcraft@gmail.com>
2024-12-18 00:08:34 -0800

    did secrets challenge

--- elixir/lasagna/lib/lasagna.ex
+++ elixir/lasagna/lib/lasagna.ex
@@ -1,6 +1,8 @@
 defmodule Lasagna do
   # Please define the 'expected_minutes_in_oven/0' function
-  def expected_minutes_in_oven(), do: 40
+  def expected_minutes_in_oven() do
+    40
+  end
   
   # Please define the 'remaining_minutes_in_oven/1' function
   def remaining_minutes_in_oven(actual) do
