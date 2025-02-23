ffb1c5e33e71324354c851ec98dceab14eb6c018
Chris Chalcraft <chris.chalcraft@gmail.com>
2025-02-22 22:32:50 -0800

    finished calculator

--- elixir/kitchen-calculator/lib/kitchen_calculator.ex
+++ elixir/kitchen-calculator/lib/kitchen_calculator.ex
@@ -24,15 +24,50 @@ defmodule KitchenCalculator do
   """
   def get_volume({_unit, volume}), do: volume
 
-  def to_milliliter(volume_pair) do
-    # Please implement the to_milliliter/1 functions
+  @doc """
+  Converts a volume-pair tuple to milliliters.
+
+  ## Examples
+
+      iex> KitchenCalculator.to_milliliter({:cup, 2.5})
+      {:milliliter, 600.0}
+
+      iex> KitchenCalculator.to_milliliter({:teaspoon, 3})
+      {:milliliter, 15.0}
+  """
+  def to_milliliter({unit, volume}) do
+    {:milliliter, volume * @conversion_factors[unit]}
   end
 
-  def from_milliliter(volume_pair, unit) do
-    # Please implement the from_milliliter/2 functions
+  @doc """
+  Converts a volume from milliliters to the desired unit.
+
+  ## Examples
+
+      iex> KitchenCalculator.from_milliliter({:milliliter, 240}, :cup)
+      {:cup, 1.0}
+
+      iex> KitchenCalculator.from_milliliter({:milliliter, 30}, :fluid_ounce)
+      {:fluid_ounce, 1.0}
+  """
+  def from_milliliter({:milliliter, volume}, desired_unit) do
+    {desired_unit, volume / @conversion_factors[desired_unit]}
   end
 
+  @doc """
+  Converts a volume-pair tuple to another unit.
+
+  ## Examples
+
+      iex> KitchenCalculator.convert({:cup, 2}, :fluid_ounce)
+      {:fluid_ounce, 16.0}
+
+      iex> KitchenCalculator.convert({:teaspoon, 3}, :tablespoon)
+      {:tablespoon, 1.0}
+  """
   def convert(volume_pair, unit) do
-    # Please implement the convert/2 function
+    volume_pair
+    |> to_milliliter()
+    |> from_milliliter(unit)
   end
 end
